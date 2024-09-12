d8d7587c3157532a49c3df33c949b231d93d5b86
Krishna-Baldwa <95856737+Krishna-Baldwa@users.noreply.github.com>
2024-09-12 16:31:45 +0530

    Time: 64 ms (24.97%) | Memory: 19.3 MB (78.41%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int goal = nums.size()-1;
+        int step = 0;
+
+        while(goal>0){
+            int farthest = 0;
+            for(int i = nums.size()-2; i>=0; i--){
+                if(i+nums[i] >= goal){
+                    farthest = i;
+                }
+            }
+            goal = farthest;
+            step++;
+        }
+        return step;
+    }
+};
