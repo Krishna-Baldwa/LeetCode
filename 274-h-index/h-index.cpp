93c35078da572cda93fd006d8ac1a34a17ee03fa
Krishna-Baldwa <95856737+Krishna-Baldwa@users.noreply.github.com>
2024-09-12 19:34:22 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (72.18%) - LeetSync

---
+++ 274-h-index/h-index.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end());
+        int h=0;
+        int n= citations.size();
+        for(int i=0;i<citations.size();i++){
+            if(citations[i]>=citations.size()-i){
+                h= max(n-i,h);
+            }
+
+        }
+        return h;
+    }
+};
